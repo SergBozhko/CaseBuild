d0bb977982c694097a267bd45deac5117940d891
Admin <admin@Mac-mini-FrontEnd.local>
2015-11-05 10:16:31 +0300

    Add Paralax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,40 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+// Height = width
+function setH (target) {
+	target.height(target.width());
+}
+function setEach (target) {
+	target.each(function() {
+		$(this).height($(this).width());
+	});
+}
+// Paralax
+function paralax (target, value) {
+	target.css({
+		'transform' : 'translate(0%, ' + value + '%'
+	});
+}
+
+$(function() {
+	console.log('DOM ready');
+
+	var $romb = $('[class*=rt-w-]'),
+		$rombs = $('.romb-targets'),
+		$rombsBg = $('.byyd-targets-rombs-bg');
+	setEach($romb);
+
+	// Our works blocks width
+	var $wBlock = $('.work-list-block');
+	setH($wBlock);
+
+	// Window actions
+	$(window).resize(function() {
+		setH($wBlock);
+		setEach($romb);
+	});
+	$(window).scroll(function() {
+		var scrT = $(this).scrollTop();
+
+		paralax($rombs, -scrT/10);
+		paralax($rombsBg, -scrT/80);
+	});
+});
